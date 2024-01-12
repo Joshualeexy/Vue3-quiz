ad68d03262f25620f2ee660e86799513224fe7fd
Joshualeexy <oghenekevwejoshualee@gmail.com>
2024-01-12 13:11:37 +0100

    Finished layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script setup>
+import { ref, watch } from 'vue'
+import datas from './data/data.json'
+import card from './components/card.vue'
 
+const searchvalue = ref('')
+const data = ref(datas)
 
+watch(searchvalue, function () {
+  data.value = datas.filter(data=>data.name.toLowerCase().includes(searchvalue.value.toLowerCase()))
+})
 
 </script>
 
 
-  <template>
-
+<template>
+  <header class="p-10 flex gap-10">
+    <h1 class="text-2xl font-bold">Quizes</h1>
+    <label for="searchbox">
+      <input type="text" v-model="searchvalue" class="px-3 py-2 bg-gray-100 rounded-lg outline-none"
+        placeholder="search">
+      </label>
+  </header>
+  <card :data="data" />
 </template>
 
 
---
+++ src/components/card.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {defineProps} from 'vue'
+const props = defineProps(['data'])
+</script>
+
+<template>
+    <div class="grid grid-cols-2 sm:grid-cols-3 p-10 gap-10">
+      <div class=""  v-for="quiz in data" :key="quiz.id">
+          <img :src="quiz.img" alt="">
+            <div class="">
+                <h2>{{ quiz.name }}</h2>
+                <small>{{ quiz.questions.length }} questions</small>
+            </div>
+      </div>
+    </div>
+</template>
